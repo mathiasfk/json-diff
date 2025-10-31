47dce63f29344e0a64c155384abd1df5265216cf
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-10-30 21:25:46 -0300

    feat: add minification

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+  },
   optimizeDeps: {
     include: ['monaco-editor'],
   },
