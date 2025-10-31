6ee11bee2abbcb885d3797b652a2f3c05487f2cd
Mathias Kriebel <mathias.kriebel@gmail.com>
2025-10-30 21:20:02 -0300

    fix: change base url for public domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/json-diff/',
+  base: '/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'node',
-  },
   optimizeDeps: {
     include: ['monaco-editor'],
   },
